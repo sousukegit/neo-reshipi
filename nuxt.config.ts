95ff7c9ebc8ad36d78e45f9efe08a6c65cf1a874
sousukegit <greenneosoul.sousuke@gmail.com>
2024-01-01 15:55:56 +0900

    CDKデプロイ用のCloudFrontからのREFERER変数チェック

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro:{
+    //nuxt3のサーバーサイドをLambdaとしてビルド
+    preset:"aws-lambda",
+  },
   devtools: { enabled: true },
   typescript:{
     strict:true,
+  },
+  runtimeConfig:{
+    //cloudfront経由のRefereと照合するための環境変数
+    referer:process.env.REFERER,
+    //開発環境かどうかの判定で開発モードの時は照合をとばす
+    //NODE_ENVはNUXT3自信の環境変数
+    //npm rub devのときはdevelopment
+    isDev:process.env.NODE_ENV === "development"
   },
   css: ["~/assets/css/tailwind.css",],
   postcss: {
---
+++ server/middleware/originAccessValidator.ts
@@ -0,0 +1,19 @@
+//CloudFrontから渡されるはずのリファラを検証して、
+//CloudFrontを経由しないアクセスをブロック
+export default defineEventHandler(async (event) => {
+    //環境変数を取得
+    const config = useRuntimeConfig();
+    if(config.isDev){
+        //開発環境では検証しない
+        return;
+    }
+    if(event.req.headers.referer !== config.referer){
+        //refererヘッダが一致しないので
+        //CloudFrontを経由しないアクセスとみなしてエラーを返す
+        throw createError({
+            statusCode:403,
+            message:"Forbidden"
+        })
+    }
+
+})
