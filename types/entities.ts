4793405358b6d59d34340c046d604f28d6479f8d
sousukegit <greenneosoul.sousuke@gmail.com>
2024-01-09 06:34:58 +0900

    listPostのAPI機能を実装

--- types/entities.ts
+++ types/entities.ts
@@ -10,3 +10,14 @@ export type RecipeEntity = {
     //調理方法
     howToCook:string,
 }
+//掲示板入力の型
+export type BbsEntity = {
+    //Idは固定
+    id:"bbs";
+    //投稿日時
+    createAt:number;
+    //投稿内容
+    post:string;
+    //保持期間
+    ttl:number;
+}
